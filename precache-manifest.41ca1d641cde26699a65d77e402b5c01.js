433eec69fd02fec23c3c5600af0e029ee073d017
Jorge <jorgelartearteagap@gmail.com>
2021-08-03 11:52:39 -0400

    build

---
+++ precache-manifest.41ca1d641cde26699a65d77e402b5c01.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672cc9a09a4d174edbf2f0add6776f24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6337f8f481922c0eeb8",
+    "url": "/static/css/main.270b9f82.chunk.css"
+  },
+  {
+    "revision": "ee786f73a0a5063355c6",
+    "url": "/static/js/2.2c0ba274.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2c0ba274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6337f8f481922c0eeb8",
+    "url": "/static/js/main.18b11087.chunk.js"
+  },
+  {
+    "revision": "edce40b592d576612e40",
+    "url": "/static/js/runtime-main.22ff32c9.js"
+  },
+  {
+    "revision": "d2c279bb596cb8810cad41444efa4f0e",
+    "url": "/static/media/fondo.d2c279bb.svg"
+  },
+  {
+    "revision": "28ec4da087a21f6aad8b4aa5c6ee01a9",
+    "url": "/static/media/hand1.28ec4da0.png"
+  },
+  {
+    "revision": "38caac26a4248458b2e8d2501a9bc8d2",
+    "url": "/static/media/logo__oferta.38caac26.png"
+  },
+  {
+    "revision": "247e43efb5ad616d2cbf7604d9f084e8",
+    "url": "/static/media/multipago.247e43ef.png"
+  }
+]);
